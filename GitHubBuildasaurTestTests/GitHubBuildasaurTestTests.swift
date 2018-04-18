ddd23036426bb24740881c90e478fc8c5b77a8b7
Aaron <acostello@themeetgroup.com>
2018-04-17 21:15:26 -0700

    Revert "FAIL TEST TEST"
    This reverts commit 64092f90a0f6c3b159db4a981dcda235c4bd75f6.

--- GitHubBuildasaurTestTests/GitHubBuildasaurTestTests.swift
+++ GitHubBuildasaurTestTests/GitHubBuildasaurTestTests.swift
@@ -24,7 +24,6 @@ class GitHubBuildasaurTestTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssert(false, "YAY FAIL!")
     }
     
     func testPerformanceExample() {
