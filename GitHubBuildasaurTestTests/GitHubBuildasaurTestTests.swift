64092f90a0f6c3b159db4a981dcda235c4bd75f6
Aaron <acostello@themeetgroup.com>
2018-04-17 21:13:59 -0700

    FAIL TEST TEST

--- GitHubBuildasaurTestTests/GitHubBuildasaurTestTests.swift
+++ GitHubBuildasaurTestTests/GitHubBuildasaurTestTests.swift
@@ -24,6 +24,7 @@ class GitHubBuildasaurTestTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(false, "YAY FAIL!")
     }
     
     func testPerformanceExample() {
